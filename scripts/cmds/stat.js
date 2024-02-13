28d88e923e85c212d1e76214907ddf83a9b335a8
kyouhei14 <158478884+kyouhei14@users.noreply.github.com>
2024-02-13 07:39:15 +0000

    nuo br

--- scripts/cmds/stat.js
+++ scripts/cmds/stat.js
@@ -52,7 +52,7 @@ module.exports = {
       }
 
       // Assuming global.utils.getStreamFromURL(img) is correctly defined
-      const imgURL= "https://i.imgur.com/TKNF26K.jpg";
+      const imgURL= "https://i.imgur.com/ziYrsKp.gif";
       const attachment = await global.utils.getStreamFromURL(imgURL);
 
       api.sendMessage({
